62aa9325a40b3ba9200a8facc9e61cd70056a65f
Mikhail Lytaev <mikelytaev@gmail.com>
2024-01-16 18:00:01 +0300

    piers24

--- stohastic_pe/piers24/elevated_ex1.py
+++ stohastic_pe/piers24/elevated_ex1.py
@@ -4,17 +4,68 @@ logging.basicConfig(level=logging.INFO)
 
 calc(
     M_profile=RandomTrilinearDuct(
-        z1=norm(loc=50, scale=5),
-        z2=norm(loc=150, scale=5),
-        m0=norm(loc=300, scale=5),
-        m1=norm(loc=330, scale=5),
-        m2=norm(loc=310, scale=5),
+        z1=norm(loc=50, scale=3),
+        z2=norm(loc=150, scale=3),
+        m0=norm(loc=300, scale=3),
+        m1=norm(loc=330, scale=3),
+        m2=norm(loc=310, scale=3),
+        slope=norm(loc=0.15, scale=0.005)
+    ),
+    range_m=250e3,
+    height_m=300,
+    freq_hz=3E9,
+    ant_height=10,
+    file_name="elevated_duct_1.eps",
+    max_monte_carlo_iterations=100
+)
+
+calc(
+    M_profile=RandomTrilinearDuct(
+        z1=norm(loc=50, scale=3),
+        z2=norm(loc=150, scale=3),
+        m0=norm(loc=300, scale=3),
+        m1=norm(loc=330, scale=3),
+        m2=norm(loc=310, scale=3),
+        slope=norm(loc=0.15, scale=0.005)
+    ),
+    range_m=250e3,
+    height_m=300,
+    freq_hz=3E9,
+    ant_height=50,
+    file_name="elevated_duct_2.eps",
+    max_monte_carlo_iterations=100
+)
+
+calc(
+    M_profile=RandomTrilinearDuct(
+        z1=norm(loc=50, scale=3),
+        z2=norm(loc=150, scale=3),
+        m0=norm(loc=300, scale=3),
+        m1=norm(loc=330, scale=3),
+        m2=norm(loc=310, scale=3),
         slope=norm(loc=0.15, scale=0.005)
     ),
     range_m=250e3,
     height_m=300,
     freq_hz=3E9,
     ant_height=150,
-    file_name="elevated_duct_1.eps",
-    max_monte_carlo_iterations=1
+    file_name="elevated_duct_3.eps",
+    max_monte_carlo_iterations=100
+)
+
+calc(
+    M_profile=RandomTrilinearDuct(
+        z1=norm(loc=50, scale=3),
+        z2=norm(loc=150, scale=3),
+        m0=norm(loc=300, scale=3),
+        m1=norm(loc=330, scale=3),
+        m2=norm(loc=310, scale=3),
+        slope=norm(loc=0.15, scale=0.005)
+    ),
+    range_m=250e3,
+    height_m=300,
+    freq_hz=3E9,
+    ant_height=250,
+    file_name="elevated_duct_4.eps",
+    max_monte_carlo_iterations=100
 )
