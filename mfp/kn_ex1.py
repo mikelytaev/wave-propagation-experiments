12e0b844e637faa7a3b1678292f151878d6ab886
Mikhail S. Lytaev <mikelytaev@gmail.com>
2024-03-05 02:20:37 +0300

    mfp experiments

--- mfp/kn_ex1.py
+++ mfp/kn_ex1.py
@@ -1,3 +1,4 @@
+import matplotlib.pyplot as plt
 import numpy as np
 
 
@@ -7,12 +8,27 @@ from utils import *
 #logging.basicConfig(level=logging.DEBUG)
 env = Troposphere(flat=True)
 env.z_max = 150
-#env.knife_edges = [
-#    KnifeEdge(range=-200, height=70),
-#    KnifeEdge(range=1300, height=70),
-#]
 antenna = Source(freq_hz=300E6, height_m=50)
 measures = [Measure(x_m=900, height_m=h, freq_hz=antenna.freq_hz) for h in range(10, 60, 2)]
-search_area=SearchArea(min_x_m=-250, max_x_m=250)
+search_area = SearchArea(min_x_m=-250, max_x_m=250)
 calc(src=antenna, env=env, range_bounds_m=[-500, 1500], measures=measures, search_area=search_area)
+plt.savefig("kn_ex1.eps")
 
+env.knife_edges = [
+   KnifeEdge(range=-200, height=70),
+]
+calc(src=antenna, env=env, range_bounds_m=[-500, 1500], measures=measures, search_area=search_area)
+plt.savefig("kn_ex2.eps")
+
+env.knife_edges = [
+   KnifeEdge(range=200, height=70),
+]
+calc(src=antenna, env=env, range_bounds_m=[-500, 1500], measures=measures, search_area=search_area)
+plt.savefig("kn_ex3.eps")
+
+env.knife_edges = [
+   KnifeEdge(range=-200, height=70),
+   KnifeEdge(range=1300, height=70),
+]
+calc(src=antenna, env=env, range_bounds_m=[-500, 1500], measures=measures, search_area=search_area)
+plt.savefig("kn_ex4.eps")
--- mfp/utils.py
+++ mfp/utils.py
@@ -45,55 +45,67 @@ def calc(src: Source, env: Troposphere, range_bounds_m, measures: List[Measure],
     bartlett_mfp_field = rwp_mfp(measures, fields, bartlett_mfp)
     mv_mfp_field = rwp_mfp(measures, fields, mv_mfp)
 
-    show(fwd_field, bartlett_mfp_field, mv_mfp_field, src, measures)
+    show(fwd_field, bartlett_mfp_field, mv_mfp_field, src, measures, env)
+    print(f'Bartlett {bartlett_mfp_field.argmax()}')
+    print(f'MV {mv_mfp_field.argmax()}')
 
     return fwd_field, bartlett_mfp_field, mv_mfp_field
 
 
-def show(fwd_field, bartlett_mfp_field, mv_mfp_field, src: Source, measures: List[Measure]):
+def show(fwd_field, bartlett_mfp_field, mv_mfp_field, src: Source, measures: List[Measure],
+         env: Troposphere):
     plt.rcParams['font.size'] = '13'
     f, ax = plt.subplots(1, 3, figsize=(9, 3.5), constrained_layout=True)
 
     field = 10 * np.log10(np.abs(fwd_field.field.T[::-1, :]))
     f_min, f_max = np.min(field), np.max(field)
     norm_fwd = Normalize(f_max-40, f_max)
-    extent_fwd = [fwd_field.x_grid[0] * 1E-3, fwd_field.x_grid[-1] * 1E-3, fwd_field.z_grid[0], fwd_field.z_grid[-1]]
+    extent_fwd = [fwd_field.x_grid[0], fwd_field.x_grid[-1], fwd_field.z_grid[0], fwd_field.z_grid[-1]]
     im = ax[0].imshow(field, extent=extent_fwd, norm=norm_fwd, aspect='auto', cmap=plt.get_cmap('jet'))
     f.colorbar(im, ax=ax[0], fraction=0.046, location='bottom')
-    ax[0].set_xlabel("Range (km)", fontsize=13)
+    ax[0].set_xlabel("Range (m)", fontsize=13)
     ax[0].set_ylabel("Height (m)", fontsize=13)
     ax[0].set_title("10log(|u|), dB", fontsize=13)
     ax[0].grid(True)
     ax[0].plot(0, src.height_m, '*', color='white')
     for m in measures:
-        ax[0].plot(m.x_m*1e-3, m.height_m, '*', color='black')
+        ax[0].plot(m.x_m, m.height_m, '*', color='black')
 
     field = 10 * np.log10(np.abs(bartlett_mfp_field.field.T[::-1, :]))
     f_min, f_max = np.min(field), np.max(field)
     norm_bartlett = Normalize(f_max - 5, f_max)
-    extent_bartlett = [bartlett_mfp_field.x_grid[0] * 1E-3, bartlett_mfp_field.x_grid[-1] * 1E-3,
+    extent_bartlett = [bartlett_mfp_field.x_grid[0], bartlett_mfp_field.x_grid[-1],
                        bartlett_mfp_field.z_grid[0], bartlett_mfp_field.z_grid[-1]]
     im = ax[1].imshow(field, extent=extent_bartlett, norm=norm_bartlett, aspect='auto', cmap=plt.get_cmap('jet'))
     f.colorbar(im, ax=ax[1], fraction=0.046, location='bottom')
-    ax[1].set_xlabel("Range (km)", fontsize=13)
+    ax[1].set_xlabel("Range (m)", fontsize=13)
     ax[1].set_yticklabels([])
     ax[1].set_title("Bartlett, dB", fontsize=13)
     ax[1].grid(True)
+    x_src, z_src = bartlett_mfp_field.argmax()
+    ax[1].plot(x_src, z_src, '*', color='white')
 
     field = 10 * np.log10(np.abs(mv_mfp_field.field.T[::-1, :]))
     f_min, f_max = np.min(field), np.max(field)
     norm_mv = Normalize(f_max - 5, f_max)
-    extent_mv = [mv_mfp_field.x_grid[0] * 1E-3, mv_mfp_field.x_grid[-1] * 1E-3, mv_mfp_field.z_grid[0],
+    extent_mv = [mv_mfp_field.x_grid[0], mv_mfp_field.x_grid[-1], mv_mfp_field.z_grid[0],
                  mv_mfp_field.z_grid[-1]]
     im = ax[2].imshow(field, extent=extent_mv, norm=norm_mv, aspect='auto', cmap=plt.get_cmap('jet'))
     f.colorbar(im, ax=ax[2], fraction=0.046, location='bottom')
-    ax[2].set_xlabel("Range (km)", fontsize=13)
+    ax[2].set_xlabel("Range (m)", fontsize=13)
     ax[2].set_yticklabels([])
     ax[2].set_title("MV, dB", fontsize=13)
     ax[2].grid(True)
+    x_src, z_src = bartlett_mfp_field.argmax()
+    ax[2].plot(x_src, z_src, '*', color='white')
+
+    for kn in env.knife_edges:
+        ax[0].axvline(x=kn.range, ymin=0, ymax=kn.height/fwd_field.z_grid[-1], color='brown')
+        if bartlett_mfp_field.x_grid[0] <= kn.range <= bartlett_mfp_field.x_grid[-1]:
+            ax[1].axvline(x=kn.range, ymin=0, ymax=kn.height/bartlett_mfp_field.z_grid[-1], color='brown')
+        if mv_mfp_field.x_grid[0] <= kn.range <= mv_mfp_field.x_grid[-1]:
+            ax[2].axvline(x=kn.range, ymin=0, ymax=kn.height/mv_mfp_field.z_grid[-1], color='brown')
 
     for a in ax[:]:
         for label in (a.get_xticklabels() + a.get_yticklabels()):
             label.set_fontsize(13)
-
-    plt.show()
