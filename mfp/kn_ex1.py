acf7909560940ee2e4477408add453572a6140a1
Mikhail Lytaev <mikelytaev@gmail.com>
2024-03-04 19:08:48 +0300

    mfp experiments

--- mfp/kn_ex1.py
+++ mfp/kn_ex1.py
@@ -1,142 +1,18 @@
-import os
-
 import numpy as np
 
-os.chdir('../../')
-from rwp.kediffraction import *
-from rwp.antennas import *
-from rwp.environment import *
-from rwp.vis import *
 
-logging.basicConfig(level=logging.DEBUG)
+from utils import *
+
+
+#logging.basicConfig(level=logging.DEBUG)
 env = Troposphere(flat=True)
 env.z_max = 150
-env.knife_edges = [
-    KnifeEdge(range=-200, height=70),
-    KnifeEdge(range=1300, height=70),
-]
-antenna = Source(wavelength=1, height_m=50)
-#antenna = GaussAntenna(freq_hz=300e6, height=100, beam_width=15, elevation_angle=0, polarz='H')
+#env.knife_edges = [
+#    KnifeEdge(range=-200, height=70),
+#    KnifeEdge(range=1300, height=70),
+#]
+antenna = Source(freq_hz=300E6, height_m=50)
+measures = [Measure(x_m=900, height_m=h, freq_hz=antenna.freq_hz) for h in range(10, 60, 2)]
+search_area=SearchArea(min_x_m=-250, max_x_m=250)
+calc(src=antenna, env=env, range_bounds_m=[-500, 1500], measures=measures, search_area=search_area)
 
-kdc = KnifeEdgeDiffractionCalculator(src=antenna, env=env, min_range_m=-500, max_range_m=1500, max_propagation_angle=90)
-field = kdc.calculate()
-
-vis = FieldVisualiser(field, env=env, trans_func=lambda v: 10 * cm.log10(1e-16 + abs(v)), label='ke')
-plt = vis.plot2d(min=-40, max=10)
-plt.title('The intensity of the field component 10log10|u|')
-plt.xlabel('Range (m)')
-plt.ylabel('Height (m)')
-plt.show()
-
-
-@dataclass
-class Measure:
-    x_m: float
-    height_m: float
-    value: complex
-    freq_hz: float
-
-
-@dataclass
-class SearchArea:
-    min_x_m: float
-    max_x_m: float
-    min_z_m: float = None
-    max_x_m: float = None
-
-
-def bartlett_mfp(measures: List[Measure], fields: List[np.ndarray]) -> np.ndarray:
-    res = deepcopy(fields[0])
-    res *= 0
-    normalizer = deepcopy(fields[0])
-    normalizer *= 0
-    for ind in range(0, len(fields)):
-        res += measures[ind].value.conjugate() * fields[ind]
-        normalizer += abs(fields[ind]) ** 2
-
-    res = res**2 / normalizer
-    return res
-
-
-def mv_mfp(measures: List[Measure], fields: List[np.ndarray]) -> np.ndarray:
-    res = deepcopy(fields[0])
-    res *= 0
-    d = np.array([m.value for m in measures], dtype=complex)
-    k = np.matmul(d.reshape(len(d), 1), d.reshape(1, len(d)).conj())
-    k_inv = np.linalg.inv(k)
-
-    normalizer = deepcopy(fields[0])
-    normalizer *= 0
-    for ind in range(0, len(fields)):
-        normalizer += abs(fields[ind]) ** 2
-
-    normalizer = np.sqrt(normalizer)
-
-    for ind_i in range(0, res.shape[0]):
-        for ind_j in range(0, res.shape[1]):
-            w = np.array([f[ind_i, ind_j] for f in fields]) / normalizer[ind_i, ind_j]
-            w = w.reshape(len(w), 1)
-            t = np.matmul(np.matmul(w.reshape(1, len(w)).conj(), k_inv), w)
-            res[ind_i, ind_j] = (1 / t) / (1 / np.matmul(w.reshape(1, len(w)).conj(), w))
-
-    return res
-
-
-def rwp_mfp(measures: List[Measure], fields: List[Field], mfp_func) -> Field:
-    np_fields = [f.field for f in fields]
-    mfp_field = deepcopy(fields[0])
-    mfp_np_field = mfp_func(measures=measures, fields=np_fields)
-    mfp_field.field = mfp_np_field
-    return mfp_field
-
-
-def mfp(measures: List[Measure], env: Troposphere, search_area: SearchArea) -> List[Field]:
-    fields = []
-    for measure in measures:
-        antenna = PointSource(freq_hz=measure.freq_hz, height_m=measure.height_m)
-        shifted_env = deepcopy(env)
-        shifted_search_area = deepcopy(search_area)
-        for knife_edge in shifted_env.knife_edges:
-            knife_edge.range -= measure.x_m
-        shifted_search_area.min_x_m -= measure.x_m
-        shifted_search_area.max_x_m -= measure.x_m
-        kdc = KnifeEdgeDiffractionCalculator(src=antenna, env=shifted_env,
-                                             min_range_m=shifted_search_area.min_x_m,
-                                             max_range_m=shifted_search_area.max_x_m,
-                                             inverse=True
-                                             )
-        field = kdc.calculate()
-        field.x_grid += measure.x_m
-        fields.append(field)
-    return fields
-
-measures = [Measure(x_m=900, height_m=h, value=field.value(900, h), freq_hz=antenna.freq_hz) for h in range(10, 60, 2)]
-
-fields = mfp(measures=measures, env=env, search_area=SearchArea(
-    min_x_m=-250,
-    max_x_m=250
-))
-
-
-vis = FieldVisualiser(fields[0], env=env, trans_func=lambda v: 10 * cm.log10(1e-16 + abs(v)), label='ke')
-plt = vis.plot2d(min=-40, max=10)
-plt.title('The intensity of the field component 10log10|u|')
-plt.xlabel('Range (m)')
-plt.ylabel('Height (m)')
-plt.show()
-
-bartlett_mfp_field = rwp_mfp(measures, fields, bartlett_mfp)
-vis = FieldVisualiser(bartlett_mfp_field, env=env, trans_func=lambda v: 10 * cm.log10(1e-16 + abs(v)), label='ke')
-plt = vis.plot2d(min=vis.max-5, max=vis.max)
-plt.title('The intensity of the field component 10log10|u|')
-plt.xlabel('Range (m)')
-plt.ylabel('Height (m)')
-plt.show()
-
-mv_mfp_field = rwp_mfp(measures, fields, mv_mfp)
-vis = FieldVisualiser(mv_mfp_field, env=env, trans_func=lambda v: 10 * cm.log10(1e-66 + abs(v)), label='ke')
-plt = vis.plot2d(min=vis.max-10, max=vis.max)
-plt.title('The intensity of the field component 10log10|u|')
-plt.xlabel('Range (m)')
-plt.ylabel('Height (m)')
-plt.show()
---
+++ mfp/utils.py
@@ -0,0 +1,99 @@
+from matplotlib.colors import Normalize
+
+from propagators.mfp import Measure, SearchArea, bartlett_mfp, mv_mfp
+from rwp.kediffraction import *
+from rwp.vis import *
+
+def rwp_mfp(measures: List[Measure], fields: List[Field], mfp_func) -> Field:
+    np_fields = [f.field for f in fields]
+    mfp_field = deepcopy(fields[0])
+    mfp_np_field = mfp_func(measures=measures, fields=np_fields)
+    mfp_field.field = mfp_np_field
+    return mfp_field
+
+
+def fields_from_measures(measures: List[Measure], env: Troposphere, search_area: SearchArea) -> List[Field]:
+    fields = []
+    for ind_m, measure in enumerate(measures):
+        antenna = PointSource(freq_hz=measure.freq_hz, height_m=measure.height_m)
+        shifted_env = deepcopy(env)
+        shifted_search_area = deepcopy(search_area)
+        for knife_edge in shifted_env.knife_edges:
+            knife_edge.range -= measure.x_m
+        shifted_search_area.min_x_m -= measure.x_m
+        shifted_search_area.max_x_m -= measure.x_m
+        kdc = KnifeEdgeDiffractionCalculator(src=antenna, env=shifted_env,
+                                             min_range_m=shifted_search_area.min_x_m,
+                                             max_range_m=shifted_search_area.max_x_m,
+                                             inverse=True
+                                             )
+        field = kdc.calculate()
+        field.x_grid += measure.x_m
+        fields.append(field)
+        print(f'{ind_m / len(measures) * 100}%')
+    return fields
+
+
+def calc(src: Source, env: Troposphere, range_bounds_m, measures: List[Measure], search_area :SearchArea):
+    kdc = KnifeEdgeDiffractionCalculator(src=src, env=env, min_range_m=range_bounds_m[0],
+                                         max_range_m=range_bounds_m[1], max_propagation_angle=90)
+    fwd_field = kdc.calculate()
+    for measure in measures:
+        measure.value = fwd_field.value(measure.x_m, measure.height_m)
+
+    fields = fields_from_measures(measures=measures, env=env, search_area=search_area)
+    bartlett_mfp_field = rwp_mfp(measures, fields, bartlett_mfp)
+    mv_mfp_field = rwp_mfp(measures, fields, mv_mfp)
+
+    show(fwd_field, bartlett_mfp_field, mv_mfp_field, src, measures)
+
+    return fwd_field, bartlett_mfp_field, mv_mfp_field
+
+
+def show(fwd_field, bartlett_mfp_field, mv_mfp_field, src: Source, measures: List[Measure]):
+    plt.rcParams['font.size'] = '13'
+    f, ax = plt.subplots(1, 3, figsize=(9, 3.5), constrained_layout=True)
+
+    field = 10 * np.log10(np.abs(fwd_field.field.T[::-1, :]))
+    f_min, f_max = np.min(field), np.max(field)
+    norm_fwd = Normalize(f_max-40, f_max)
+    extent_fwd = [fwd_field.x_grid[0] * 1E-3, fwd_field.x_grid[-1] * 1E-3, fwd_field.z_grid[0], fwd_field.z_grid[-1]]
+    im = ax[0].imshow(field, extent=extent_fwd, norm=norm_fwd, aspect='auto', cmap=plt.get_cmap('jet'))
+    f.colorbar(im, ax=ax[0], fraction=0.046, location='bottom')
+    ax[0].set_xlabel("Range (km)", fontsize=13)
+    ax[0].set_ylabel("Height (m)", fontsize=13)
+    ax[0].set_title("10log(|u|), dB", fontsize=13)
+    ax[0].grid(True)
+    ax[0].plot(0, src.height_m, '*', color='white')
+    for m in measures:
+        ax[0].plot(m.x_m*1e-3, m.height_m, '*', color='black')
+
+    field = 10 * np.log10(np.abs(bartlett_mfp_field.field.T[::-1, :]))
+    f_min, f_max = np.min(field), np.max(field)
+    norm_bartlett = Normalize(f_max - 5, f_max)
+    extent_bartlett = [bartlett_mfp_field.x_grid[0] * 1E-3, bartlett_mfp_field.x_grid[-1] * 1E-3,
+                       bartlett_mfp_field.z_grid[0], bartlett_mfp_field.z_grid[-1]]
+    im = ax[1].imshow(field, extent=extent_bartlett, norm=norm_bartlett, aspect='auto', cmap=plt.get_cmap('jet'))
+    f.colorbar(im, ax=ax[1], fraction=0.046, location='bottom')
+    ax[1].set_xlabel("Range (km)", fontsize=13)
+    ax[1].set_yticklabels([])
+    ax[1].set_title("Bartlett, dB", fontsize=13)
+    ax[1].grid(True)
+
+    field = 10 * np.log10(np.abs(mv_mfp_field.field.T[::-1, :]))
+    f_min, f_max = np.min(field), np.max(field)
+    norm_mv = Normalize(f_max - 5, f_max)
+    extent_mv = [mv_mfp_field.x_grid[0] * 1E-3, mv_mfp_field.x_grid[-1] * 1E-3, mv_mfp_field.z_grid[0],
+                 mv_mfp_field.z_grid[-1]]
+    im = ax[2].imshow(field, extent=extent_mv, norm=norm_mv, aspect='auto', cmap=plt.get_cmap('jet'))
+    f.colorbar(im, ax=ax[2], fraction=0.046, location='bottom')
+    ax[2].set_xlabel("Range (km)", fontsize=13)
+    ax[2].set_yticklabels([])
+    ax[2].set_title("MV, dB", fontsize=13)
+    ax[2].grid(True)
+
+    for a in ax[:]:
+        for label in (a.get_xticklabels() + a.get_yticklabels()):
+            label.set_fontsize(13)
+
+    plt.show()
